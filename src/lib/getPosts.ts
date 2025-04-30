1391e5019e5c2fb72bedcb54627bcf2d17455404
Eugene Boruhov <eugene@focusreactive.com>
2025-04-30 18:55:34 +0200

    Add `hardhat-ignition` tag

--- src/lib/getPosts.ts
+++ src/lib/getPosts.ts
@@ -1,6 +1,6 @@
 export default async function getPosts() {
   const API_KEY = process.env.NEXT_GHOST_CMS_API_KEY;
-  const endpoint = `${process.env.NEXT_GHOST_CMS_CONTENT_URL}/posts?key=${API_KEY}&filter=tag:hardhat`;
+  const endpoint = `${process.env.NEXT_GHOST_CMS_CONTENT_URL}/posts?key=${API_KEY}&filter=tag:hardhat,hardhat-ignition`;
 
   const response = await fetch(endpoint);
 
