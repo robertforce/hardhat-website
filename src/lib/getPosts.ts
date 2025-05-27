3a67d6434300ca39e721b319de884eb5dc1c2d05
Franco Zeoli <zeolifranco@gmail.com>
2025-05-27 17:52:34 -0300

    Remove console.log

--- src/lib/getPosts.ts
+++ src/lib/getPosts.ts
@@ -1,7 +1,7 @@
 export default async function getPosts() {
   const API_KEY = process.env.NEXT_GHOST_CMS_API_KEY;
   const endpoint = `${process.env.NEXT_GHOST_CMS_CONTENT_URL}/posts?key=${API_KEY}&filter=tag:[hardhat,hardhat-ignition]`;
-  console.log(endpoint)
+  
   const response = await fetch(endpoint);
 
   if (!response.ok) {
