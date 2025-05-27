ee9a0f2a8941da558b743f437b8d5335eee5e553
Franco Zeoli <zeolifranco@gmail.com>
2025-05-27 17:47:42 -0300

    Change failure more for blog post fetching

--- src/lib/getPosts.ts
+++ src/lib/getPosts.ts
@@ -1,14 +1,14 @@
 export default async function getPosts() {
   const API_KEY = process.env.NEXT_GHOST_CMS_API_KEY;
   const endpoint = `${process.env.NEXT_GHOST_CMS_CONTENT_URL}/posts?key=${API_KEY}&filter=tag:[hardhat,hardhat-ignition]`;
-
+  console.log(endpoint)
   const response = await fetch(endpoint);
 
   if (!response.ok) {
-    console.error(`HTTP error! Status: ${response.status}`);
-    return [];
+    throw new Error(`HTTP error! Status: ${response.status}`);
   }
 
   const data = await response.json();
+
   return data.posts.slice(0, 3);
 }
