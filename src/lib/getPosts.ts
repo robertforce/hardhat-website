00885b4fababbcbab678ac5fb9d8c903fb5dafef
Patricio Palladino <email@patriciopalladino.com>
2025-07-07 18:33:05 +0000

    Fix CI

--- src/lib/getPosts.ts
+++ src/lib/getPosts.ts
@@ -1,6 +1,18 @@
 export default async function getPosts() {
   const API_KEY = process.env.NEXT_GHOST_CMS_API_KEY;
-  const endpoint = `${process.env.NEXT_GHOST_CMS_CONTENT_URL}/posts?key=${API_KEY}&filter=tag:[hardhat,hardhat-ignition]`;
+  const CONTENT_URL = process.env.NEXT_GHOST_CMS_CONTENT_URL;
+
+  if (!API_KEY || !CONTENT_URL) {
+    if (!process.env.VERCEL_ENV) {
+      return [];
+    }
+
+    throw new Error(
+      `Please set NEXT_GHOST_CMS_API_KEY and NEXT_GHOST_CMS_CONTENT_URL environment variables`
+    );
+  }
+
+  const endpoint = `${CONTENT_URL}/posts?key=${API_KEY}&filter=tag:[hardhat,hardhat-ignition]`;
 
   const response = await fetch(endpoint);
 
