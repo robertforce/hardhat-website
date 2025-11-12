020df3ddaebed3b02c4ae6e9ece86856ad864b91
Franco Victorio <victorio.franco@gmail.com>
2025-11-12 16:55:36 +0000

    Collapse tutorial by default

--- astro.config.ts
+++ astro.config.ts
@@ -83,6 +83,7 @@ export default defineConfig({
                 },
                 {
                   label: "Tutorial",
+                  collapsed: true,
                   autogenerate: {
                     directory: "docs/tutorial",
                   },
